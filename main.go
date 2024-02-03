4778d98474f05a6a7ad3f18f7481429ec8b3c84c
Tyler Smith <tylerlwsmith@gmail.com>
2024-02-02 22:57:47 -0800

    Install templ

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/a-h/templ"
+
+	"bakersfieldtechnology.com/components"
+)
+
+func main() {
+	component := components.Hello("Tyler")
+
+	http.Handle("/", templ.Handler(component))
+
+	fmt.Println("Listening on :3005")
+	http.ListenAndServe(":3005", nil)
+}
