b921d70a001277fdb5320c53e96354a8e55ed91d
Tyler Smith <tylerlwsmith@gmail.com>
2024-02-24 23:56:56 -0800

    Assets working in dev mode

--- assets/assetsdev.go
+++ assets/assetsdev.go
@@ -5,10 +5,13 @@ package assets
 import (
 	"io/fs"
 	"log"
+	"net/http"
 	"os"
 )
 
-var TmplFiles fs.FS
+var AssetsHandler http.Handler
+var PublicFiles fs.FS
+var viteOrigin string = "http://localhost:5173"
 
 func init() {
 	dir, err := os.Getwd()
@@ -18,4 +21,21 @@ func init() {
 	}
 
 	PublicFiles = os.DirFS(dir + "/public")
+
+	AssetsHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(404)
+	})
+
+	viteOriginEnv := os.Getenv("VITE_ORIGIN")
+	if viteOriginEnv != "" {
+		viteOrigin = viteOriginEnv
+	}
+
+	if viteOrigin[len(viteOrigin)-1:] != "/" {
+		viteOrigin = viteOrigin + "/"
+	}
+}
+
+func Get(src string) (resolved string, err error) {
+	return viteOrigin + "assets/" + src, err
 }
