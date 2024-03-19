98ca42ec2e548b0b34b90c3606b8fad9108f5e8e
Tyler Smith <tylerlwsmith@gmail.com>
2024-03-18 19:05:34 -0700

    Reorder app.ts

--- assets/scripts/app.ts
+++ assets/scripts/app.ts
@@ -1,9 +1,9 @@
 import { scrollToAnchor } from "./scroll-to-achor";
 
-for (const link of document.querySelectorAll("a")) {
-  link.addEventListener("click", scrollToAnchor);
-}
-
 console.log(
   "Want to view the source for this site? Go to https://github.com/tylerlwsmith/go-bakersfieldtechnology"
 );
+
+for (const link of document.querySelectorAll("a")) {
+  link.addEventListener("click", scrollToAnchor);
+}
