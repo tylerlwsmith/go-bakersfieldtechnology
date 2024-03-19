da35058672c1085cca9c0692637cdf412beb80e2
Tyler Smith <tylerlwsmith@gmail.com>
2024-03-18 18:01:07 -0700

    Add scroll-to-anchor feature

---
+++ assets/scripts/scroll-to-achor.ts
@@ -0,0 +1,30 @@
+export function scrollToAnchor(event: MouseEvent) {
+  if (!(event.target instanceof HTMLAnchorElement)) return;
+
+  const link = new URL(event.target.href);
+  if (link.origin !== location.origin || link.pathname !== location.pathname) {
+    return;
+  }
+
+  const targetId = link.hash;
+  if (targetId === "") return;
+
+  const section = document.querySelector(targetId);
+  if (!(section instanceof HTMLElement)) return;
+
+  event.preventDefault();
+  const hasTabIndex = section.hasAttribute("tabindex");
+  const hasOutline = section.style.outline ? true : false;
+
+  if (!hasTabIndex) section.setAttribute("tabindex", "-1");
+  if (!hasOutline) section.style.outline = "none";
+
+  section.scrollIntoView({ behavior: "smooth", block: "start" });
+  section.focus({ preventScroll: true });
+
+  section.addEventListener("blur", function blurListener() {
+    section.removeEventListener("blur", blurListener);
+    if (!hasTabIndex) section.removeAttribute("tabindex");
+    if (!hasOutline) section.style.removeProperty("outline");
+  });
+}
