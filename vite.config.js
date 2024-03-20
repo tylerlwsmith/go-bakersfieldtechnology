5313d6bcab33be42e40f72844ae960b2d68f1519
Tyler Smith <tylerlwsmith@gmail.com>
2024-03-19 21:46:34 -0700

    Vite empty outDir and disable publicDir

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,12 @@ import { defineConfig } from "vite";
 export default defineConfig({
   root: "assets/",
   base: "/assets/",
+  publicDir: false,
   build: {
     ourDir: "dist",
     manifest: "manifest.json",
     assetsDir: "public",
+    emptyOutDir: true,
     rollupOptions: {
       input: ["assets/scripts/app.ts", "assets/styles/app.scss"],
     },
