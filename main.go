4b0a561696c682db6c73d954b5af546f965e1d47
Tyler Smith <tylerlwsmith@gmail.com>
2024-04-12 00:31:40 -0700

    Add error page, trailing slash middleware & change embeds

--- main.go
+++ main.go
@@ -6,15 +6,37 @@ import (
 	"strings"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 
 	"bakersfieldtechnology.com/assets"
 	"bakersfieldtechnology.com/components"
+	// "bakersfieldtechnology.com/components/errorpage"
 	"bakersfieldtechnology.com/components/homepage"
 	"bakersfieldtechnology.com/components/privacypolicy"
 )
 
 func main() {
 	app := echo.New()
+	app.Use(middleware.AddTrailingSlashWithConfig(middleware.TrailingSlashConfig{
+		RedirectCode: http.StatusMovedPermanently,
+	}))
+
+	// https://echo.labstack.com/docs/error-handling#error-pages
+	customHTTPErrorHandler := func(err error, c echo.Context) {
+		// code := http.StatusInternalServerError
+		// if he, ok := err.(*echo.HTTPError); ok {
+		// 	code = he.Code
+		// }
+
+		c.HTML(404, "hello world!")
+
+		// if(code == 404) {
+		// 	return components.Render(c, code, errorpage.NotFound())
+		// }
+		//
+		// return component.Render(c, code, errorpage.ServerError())
+	}
+	app.HTTPErrorHandler = customHTTPErrorHandler
 
 	app.GET("/assets/public/*", echo.WrapHandler(http.StripPrefix("/assets/public", assets.AssetsHandler)))
 	app.GET("/", func(c echo.Context) error {
@@ -25,9 +47,12 @@ func main() {
 		return components.Render(c, http.StatusOK, privacypolicy.PrivacyPolicy())
 	})
 
-	// TODO: should I export a handler for public files? There would be more
-	// symmetry with the AssetHandler if I did.
-	app.GET("/*", echo.WrapHandler(http.FileServer(http.FS(assets.PublicFiles))))
+	// https://pkg.go.dev/github.com/labstack/echo/v4@v4.11.4#Echo.StaticFS
+	app.StaticFS("/*", assets.PublicFiles)
+
+	app.RouteNotFound("/*", func(c echo.Context) error {
+		return components.Render(c, http.StatusOK, homepage.Homepage())
+	})
 
 	port, ok := os.LookupEnv("APP_PORT")
 	if !ok {
